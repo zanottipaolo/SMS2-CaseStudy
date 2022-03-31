70225bfd222c8aa8d171de63fc233ecae1ccae9d
Jfkmdd <federico.deduro@gmail.com>
2022-03-31 15:54:09 +0200

    Aggiunta nomi corr

--- script.m
+++ script.m
@@ -11,16 +11,25 @@ tSud = T(:, 20:25);
 tIsole = T(:, 26:31);
 
 %% Matrice di correlazione
-NO_corr = corr(tNordOvest{:,:}, 'rows','complete')
-NE_corr = corr(tNordEst{:,:}, 'rows','complete')
-CE_corr = corr(tCentro{:,:}, 'rows','complete')
-SU_corr = corr(tSud{:,:}, 'rows','complete')
-IS_corr = corr(tIsole{:,:}, 'rows', 'complete')
+NO_corr = array2table(corr(tNordOvest{:,:}, 'rows','complete'))
+NO_corr.Properties.VariableNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+NO_corr.Properties.RowNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
 
-corr(tNordEst{:,:}, 'rows','complete')
-corr(tCentro{:,:}, 'rows','complete')
-corr(tSud{:,:}, 'rows','complete')
-corr(tIsole{:,:}, 'rows','complete')
+NE_corr = array2table(corr(tNordEst{:,:}, 'rows','complete'))
+NE_corr.Properties.VariableNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+NE_corr.Properties.RowNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+
+CE_corr = array2table(corr(tCentro{:,:}, 'rows','complete'))
+CE_corr.Properties.VariableNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+CE_corr.Properties.RowNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+
+SU_corr = array2table(corr(tSud{:,:}, 'rows','complete'))
+SU_corr.Properties.VariableNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+SU_corr.Properties.RowNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+
+IS_corr = array2table(corr(tIsole{:,:}, 'rows','complete'))
+IS_corr.Properties.VariableNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
+IS_corr.Properties.RowNames = {'Diabete', 'Ipertensione', 'Tumori', 'Fumatori', 'Peso', 'Alcool'}
 
 %% Plot casi di diabete in Italia %%
 figure
